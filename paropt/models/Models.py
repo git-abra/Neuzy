4b42961a928b40285d28338c6441643ee463f042
Fanantenana <fanantenana@fanantenana.de>
2022-05-17 11:18:46 +0200

    added placeholder classes for OO

---
+++ paropt/models/Models.py
@@ -0,0 +1,22 @@
+#### Neuzy
+
+## Classes for models
+
+
+class GenModel():
+    """ Model class for general properties """
+    def __init__():
+        pass
+
+
+class HocModel(GenModel):
+    """ HocModel to create a Model automatically from extracting information out of an existing HOC model """
+    def __init__():
+        pass
+
+
+
+class PyModel(GenModel):
+    """ PyModel to create a NEURON model in Python """
+    def __init__():
+        pass
